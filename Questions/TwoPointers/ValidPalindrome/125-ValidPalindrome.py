4b1c1d59d7e257b5542645f4dcc74249cc3bb11e
Yejin Park <o0o2211915@gmail.com>
2022-11-11 10:09:14 +0900

    [yejin] add solution using two pointer

--- Questions/TwoPointers/ValidPalindrome/125-ValidPalindrome.py
+++ Questions/TwoPointers/ValidPalindrome/125-ValidPalindrome.py
@@ -6,3 +6,17 @@ class Solution:
     def isPalindrome2(self, s: str) -> bool:
         target = re.sub('[^a-z0-9]', '', s.lower())
         return target == target[::-1]
+
+    def isPalindrome3(self, s : str) -> bool:
+        target = s.lower()
+        left, right = 0, len(target)-1
+        while left < right:
+            while left < right and not target[left].isalnum():
+                left += 1
+            while left < right and not target[right].isalnum():
+                right -= 1
+            if target[left] != target[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
