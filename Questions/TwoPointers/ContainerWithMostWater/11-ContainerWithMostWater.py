6fa8c4ee65c2b2ba4dcac28adcef4bf48c08ab50
Yejin Park <o0o2211915@gmail.com>
2022-11-11 10:18:28 +0900

    [yejin] leetcode 11 container with most water

---
+++ Questions/TwoPointers/ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left, right = 0, len(height)-1
+        volume = (right-left)*min(height[left], height[right])
+        while left<right:
+            if height[left]<height[right]:
+                left += 1
+            else:
+                right -= 1
+            volume = max(volume, (right-left)*min(height[left], height[right]))
+        return volume
